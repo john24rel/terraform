96e614d68602ae15512cdcbc0bd697773b748c09
EC2 Default User <john oshikoya>
2020-11-07 23:36:26 +0000

    class1

--- class1/group.tf
+++ class1/group.tf
@@ -1,12 +1,31 @@
-resource "aws_iam_group" "developers" {
-    name = "developers"
-    path = "/"
+resource "aws_iam_group" "Developer" {
+  name = "Developer"
+  path = "/"
 }
 
-resource "aws_iam_group_membership" "developers_team" {
-  name = "developers-group-membership"
+resource "aws_iam_group" "Management" {
+  name = "Management"
+  path = "/"
+}
+
+resource "aws_iam_group_membership" "Developer_team" {
+  name = "Developer-group-membership"
+
   users = [
-    "${aws_iam_user.tim.name}"
+    "${aws_iam_user.Tim.name}",
+    "${aws_iam_user.Bob.name}",
+    "${aws_iam_user.Ben.name}",
   ]
-  group = "${aws_iam_group.developers.name}"
+
+  group = "${aws_iam_group.Developer.name}"
+}
+
+resource "aws_iam_group_membership" "Managment_team" {
+  name = "Managment-group-membership"
+
+  users = [
+    "${aws_iam_user.Ben.name}",
+  ]
+
+  group = "${aws_iam_group.Management.name}"
 }
--- class1/user.tf
+++ class1/user.tf
@@ -1,4 +1,14 @@
-resource "aws_iam_user" "tim" {
-   name = "tim"
-   path = "/"
+resource "aws_iam_user" "Tim" {
+  name = "Tim"
+  path = "/"
+}
+
+resource "aws_iam_user" "Bob" {
+  name = "Bob"
+  path = "/"
+}
+
+resource "aws_iam_user" "Ben" {
+  name = "Ben"
+  path = "/"
 }
