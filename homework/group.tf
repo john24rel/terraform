9e9c9950256e6fc71fdf6d7842e7868a640d6c50
EC2 Default User <john oshikoya>
2020-11-10 02:29:08 +0000

    poweruser

---
+++ homework/group.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_group" "Infosec" {
+  name = "Infosec"
+  path = "/"
+}
+resource "aws_iam_group_membership" "Infosec" {
+  name = "Infosec-group-membership"
+
+  users = [
+    "${aws_iam_user.Ron.name}",
+    "${aws_iam_user.Sam.name}",
+    "${aws_iam_user.Billy.name}",
+  ]
+
+  group = "${aws_iam_group.Infosec.name}"
+}
+
+
