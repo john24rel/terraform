d301bb627bd01564c0549a2b3aedc7852ba3c29c
EC2 Default User <john oshikoya>
2020-12-11 02:44:42 +0000

    class13&14

---
+++ class14/EKS/variable.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+    default = "class14-petal-298101"
+}
+
+variable "cluster_name" {}
+variable "initial_node_count" {}
+variable "machine_type" {}
+variable "node_count" {}
+variable "location" {}
---
+++ class14/provider.tf
@@ -0,0 +1,7 @@
+provider "google-beta" {	
+version = "2.20"	
+credentials = file("~/.google/credentials")
+project = "class14-petal-298101"	
+region = "us-central1"	
+zone = "us-central1-c"
+}
