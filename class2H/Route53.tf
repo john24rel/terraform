2bc20548a9b48f7be0c949acb78e6a528ee47689
EC2 Default User <john oshikoya>
2020-12-11 21:56:57 +0000

    latest

--- class2H/Route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "org" {
-  zone_id = "Z072691037T3UF5W6Z7KL"
-  name    = "org.johnoshikoya.com"
-  type    = "A"
-  ttl     = "5"
-  records = ["${aws_instance.ubuntu.public_ip}"]
-}
---
+++ terraform-gcp-ASG-EKS/ASG/variable.tf
@@ -0,0 +1,6 @@
+variable "project_id" {
+  default = "class14-petal-298101"
+}
+variable "region" {
+    default = "us-central1"
+}
